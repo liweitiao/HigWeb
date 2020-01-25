6aae1e71c0e21832188069add9e80a97e11a4abd
18825155430 <2314205800@qq.com>
2020-01-25 10:22:07 +0800

    第一次提交

---
+++ articles/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+# Register your models here.
+# users/admin.py:
+from articles.models import Category, Entry
+
+
+admin.site.site_title = 'higreen'
+admin.site.site_header = '海吉星官网'
+admin.site.index_title = '海吉星官网后台管理系统'
+
+
+class EntryAdmin(admin.ModelAdmin):
+    # 指定要显示的属性
+    list_display = ["id", "title",  "created_time"]
+
+
+class CategoryStackedInline(admin.StackedInline):
+    model = Entry  # 关联对象类型
+
+
+class CategoryTabularInline(admin.TabularInline):
+    model = Entry  # 关联对象类型
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     # inlines = [DepartmentTabularInline]  # 栈的方式显示
+#     inlines = [DepartmentStackedInline]      # 表格样式显示
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    # 指定要显示的属性
+    list_display = ["id", "name"]
+    # inlines = [CategoryStackedInline]  # 栈的方式显示
+    # inlines = [CategoryTabularInline]      # 表格样式显示
+
+
+# 注册Model类
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Entry, EntryAdmin)
+
---
+++ articles/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, verbose_name='文章分类')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "文章分类"
+        verbose_name_plural = '文章分类'
+
+
+# class Tag(models.Model):
+#
+#     name = models.CharField(max_length=128, verbose_name='文章标签')
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         verbose_name = "文章标签"
+#         verbose_name_plural = '文章标签'
+
+
+class Entry(models.Model):
+
+    title = models.CharField(max_length=128, verbose_name='文章标题')
+
+    author = models.ForeignKey(User, verbose_name='文章作者')
+
+    img = models.ImageField(upload_to='blog_images', null=True, blank=True, verbose_name='文章配图')
+
+    body = RichTextUploadingField(verbose_name='文章正文')
+
+    abstract = models.TextField(max_length=256, blank=True, null=True, verbose_name='文章摘要')
+
+    visiting = models.PositiveIntegerField(default=0, verbose_name='文章访问量')
+
+    category = models.ManyToManyField('Category', verbose_name='文章分类')
+
+    # tags = models.ManyToManyField('Tag', verbose_name='文章+标签')
+
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    modified_time = models.DateTimeField(auto_now=True, verbose_name='修改时间')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article:article_detail', kwargs={'article_id': self.id})  # http://127.0.0.1/article/3
+
+    def increase_visiting(self):
+        self.visiting += 1
+        self.save(update_fields=['visiting'])
+
+    class Meta:
+        ordering = ['-created_time']
+        verbose_name = "文章"
+        verbose_name_plural = "文章"
